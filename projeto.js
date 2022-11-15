56758169508b0ed5e94a51b476b0230d4ce73593
jazmin pinto <jazminpinto61@gmail.com>
2022-11-14 21:57:12 -0300

    Comenzando o projeto com js 14-11

--- projeto.js
+++ projeto.js
@@ -4,3 +4,35 @@ Exibir erros
 
 Exibir repositórios do GitHub do usuário
 Consultar API*/
+function enviar(){
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let assunto = document.getElementById('assunto').value;
+    let comentario = document.getElementById('comentario').value;
+
+    let json = {
+        'nome': nome,
+        'email': email,
+        'assunto': assunto,
+        'comentario': comentario,
+    }
+    console.log(json)
+}
+function validateEmail(e) {
+    email = e.target;
+    email.Value = email.value;
+
+    if (emailValue.search('@') == 1) {
+        displayError ('Email nao é valido', email);
+    } else {
+        clearError(email);
+    }
+
+    email.clssList.remove('not-validated');
+    checkEnableSubmit();
+
+}
+console.log(validateEmail)
+
+
+document.querySelector('#butao-terciario').addEventListener('click', enviar)
