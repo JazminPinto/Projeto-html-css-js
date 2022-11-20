e4e89ac82d34053375f0666193be298e41ee485a
jazmin pinto <jazminpinto61@gmail.com>
2022-11-20 17:51:51 -0300

    Terminei o formulario

--- projeto.js
+++ projeto.js
@@ -6,18 +6,19 @@ Exibir repositórios do GitHub do usuário
 Consultar API*/
 
 function validarNome() {
+    let inputNome = document.getElementById('nome')
+    let spanNome = document.getElementById('span-nome')
     let nome = document.getElementById('nome').value;
     let tamanhoDoNome = nome.length
 
-    console.log(tamanhoDoNome)
-
-    if (tamanhoDoNome <= 1) {
-        console.log('nao pode estar vazio')
+    if (tamanhoDoNome <= 2) {
+        inputNome.classList.add('erro')
+        spanNome.innerHTML = 'nome incorreto'
         return false
 
     } else {
-
-        console.log('nome valido')
+        inputNome.classList.remove('erro')
+        spanNome.innerHTML = ''
     }
     return true
 }
@@ -26,40 +27,77 @@ function validarEmail() {
     let inputEmail = document.getElementById('email')
     let spanEmail = document.getElementById('span-email')
     let email = document.getElementById('email').value;
-    console.log(email)
 
     if (email.search('@') == -1) {
 
         inputEmail.classList.add('erro')
         spanEmail.innerHTML = 'formato incorreto'
-        console.log('formato incorreto')
         return false
 
     } else {
-        console.log('formato valido')
         inputEmail.classList.remove('erro')
         spanEmail.innerHTML = ''
     }
     return true
 }
 
+function validarAssunto() {
+    let inputAssunto = document.getElementById('assunto')
+    let spanAssunto = document.getElementById('span-assunto')
+    let assunto = document.getElementById('assunto').value
+    let tamanhoDoAssunto = assunto.length
 
 
+    if (tamanhoDoAssunto <= 1) {
+        inputAssunto.classList.add('erro')
+        spanAssunto.innerHTML = 'assunto incompleto'
+        return false
+    } else {
+        inputAssunto.classList.remove('erro')
+        spanAssunto.innerHTML = ''
+    }
+    return true
+}
+
+function validarComentario() {
+    let inputComentario = document.getElementById('comentario')
+    let spanComentario = document.getElementById('span-comentario')
+    let comentario = document.getElementById('comentario').value
+    let tamanhoDoComentario = comentario.length
 
 
+    if (tamanhoDoComentario <= 10) {
+        inputComentario.classList.add('erro')
+        spanComentario.innerHTML = 'o comentario deve ter minimo 10 letras'
+        return false
+
+    } else {
+        inputComentario.classList.remove('erro')
+        spanComentario.innerHTML = ''
+    }
+    return true
+}
 
 function enviar() {
-    // let assunto = document.getElementById('assunto').value;
-    //let comentario = document.getElementById('comentario').value;
     validarNome()
     validarEmail()
+    validarAssunto()
+    validarComentario()
 
+    if (validarNome() == true && validarEmail() == true && validarAssunto() == true && validarComentario() == true) {
+        sucesso.classList.add('sucesso')
+        sucesso.innerHTML = 'Formulario completo, Dados enviados com sucesso'
+        return true
 
-    if (validarNome() == true && validarEmail() == true) {
-        console.log('formulario correcto')
-    } else { console.log('formulario incorrecto') }
+    } else { console.log('formulario incompleto')
+    sucesso.classList.add('erro')
+    sucesso.innerHTML = 'Formulario incompleto, por favor verifique seus dados' }
+    return false
 }
 
 document.querySelector('#nome').addEventListener('input', validarNome)
 document.querySelector('#email').addEventListener('input', validarEmail)
+document.querySelector('#assunto').addEventListener('input', validarAssunto)
+document.querySelector('#comentario').addEventListener('input', validarComentario)
 document.querySelector('#botao-terciario').addEventListener('click', enviar)
+/*document.querySelector('#nome').addEventListener.('input', limpaForm)*/
