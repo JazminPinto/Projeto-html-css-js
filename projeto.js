64c8f9e1a2fa2364b435e0952897abca87676080
jazmin pinto <jazminpinto61@gmail.com>
2022-11-19 22:47:08 -0300

    iniciando o projeto do js

--- projeto.js
+++ projeto.js
@@ -4,35 +4,62 @@ Exibir erros
 
 Exibir repositórios do GitHub do usuário
 Consultar API*/
-function enviar(){
+
+function validarNome() {
     let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;
-    let assunto = document.getElementById('assunto').value;
-    let comentario = document.getElementById('comentario').value;
+    let tamanhoDoNome = nome.length
 
-    let json = {
-        'nome': nome,
-        'email': email,
-        'assunto': assunto,
-        'comentario': comentario,
-    }
-    console.log(json)
-}
-function validateEmail(e) {
-    email = e.target;
-    email.Value = email.value;
+    console.log(tamanhoDoNome)
 
-    if (emailValue.search('@') == 1) {
-        displayError ('Email nao é valido', email);
+    if (tamanhoDoNome <= 1) {
+        console.log('nao pode estar vazio')
+        return false
+
     } else {
-        clearError(email);
+
+        console.log('nome valido')
     }
-
-    email.clssList.remove('not-validated');
-    checkEnableSubmit();
-
+    return true
 }
-console.log(validateEmail)
+
+function validarEmail() {
+    let inputEmail = document.getElementById('email')
+    let spanEmail = document.getElementById('span-email')
+    let email = document.getElementById('email').value;
+    console.log(email)
+
+    if (email.search('@') == -1) {
+
+        inputEmail.classList.add('erro')
+        spanEmail.innerHTML = 'formato incorreto'
+        console.log('formato incorreto')
+        return false
+
+    } else {
+        console.log('formato valido')
+        inputEmail.classList.remove('erro')
+        spanEmail.innerHTML = ''
+    }
+    return true
+}
 
 
-document.querySelector('#butao-terciario').addEventListener('click', enviar)
+
+
+
+
+function enviar() {
+    // let assunto = document.getElementById('assunto').value;
+    //let comentario = document.getElementById('comentario').value;
+    validarNome()
+    validarEmail()
+
+
+    if (validarNome() == true && validarEmail() == true) {
+        console.log('formulario correcto')
+    } else { console.log('formulario incorrecto') }
+}
+
+document.querySelector('#nome').addEventListener('input', validarNome)
+document.querySelector('#email').addEventListener('input', validarEmail)
+document.querySelector('#botao-terciario').addEventListener('click', enviar)
